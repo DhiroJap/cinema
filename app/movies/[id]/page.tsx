85913219d5c91a69db9a529e6732c5ba68b2c1af
DhiroJap <dhiro.jap@binus.ac.id>
2023-11-28 21:44:24 +0700

    Clean up mess

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,26 +1,16 @@
-'use client';
-
-import { MovieType } from '@/utils/types';
 import MovieDetails from '@/components/movies/moviedetails';
-import mockMovies from '@/__mocks__/movies';
-import { useParams } from 'next/navigation';
 import { Page } from '@/styles';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'GALAXY CINEMA - Movie Details',
+  description: 'Movie Details Page',
+};
 
 const MovieDetailsPage = () => {
-  const params = useParams();
-  const { id } = params;
-
-  const movie: MovieType | undefined = mockMovies.find(
-    (m) => m.id === Number(id)
-  );
-
-  if (!movie) {
-    return <div>Movie not found</div>;
-  }
-
   return (
     <Page display='flex' justifyContent='center' alignItems='center'>
-      <MovieDetails movie={movie} />
+      <MovieDetails />
     </Page>
   );
 };
--- components/movies/movielist.tsx
+++ components/movies/movielist.tsx
@@ -1,21 +1,3 @@
-import { NewButton } from '@/styles';
-import { MovieType } from '@/utils/types';
-import Link from 'next/link';
-
-type Props = {
-  movies: MovieType[];
-};
-
-export default function MovieList({ movies }: Props) {
-  return (
-    <div className='flex flex-col gap-4'>
-      {movies.map((movie) => (
-        <Link href={`/movies/${movie.id}`} key={movie.id} legacyBehavior>
-          <a>
-            <NewButton className='mr-3'>{movie.title}</NewButton>
-          </a>
-        </Link>
-      ))}
-    </div>
-  );
+export default function MovieList() {
+  return <h1>Movie List</h1>;
 }
