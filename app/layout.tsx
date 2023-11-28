8ea091111a1e283a8b492d7447e6a5d67213b960
DhiroJap <dhiro.jap@binus.ac.id>
2023-11-28 22:24:46 +0700

    Create Footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { ReduxProvider } from '@/redux/provider';
 import Navbar from '@/components/shared/navbar';
+import Footer from '@/components/shared/footer';
 
 export const metadata: Metadata = {
   title: 'GALAXY CINEMA - Now Playing',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         <ReduxProvider>{children}</ReduxProvider>
+        <Footer />
       </body>
     </html>
   );
