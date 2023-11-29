19aeddf962244dcfafd7535bf54261e95412e26e
Leeyungsen <frederickmaxwellee02@gmail.com>
2023-11-29 14:45:51 +0700

    testing

--- app/Payment/payment.tsx
+++ app/Payment/payment.tsx
@@ -3,14 +3,14 @@ import { Metadata } from 'next';
 import PaymentForm from '@/components/forms/paymentform';
 
 export const metadata: Metadata = {
-    title: 'GALAXY CINEMA - Payment'
+    title: 'GALAXY CINEMA - Payment',
     description: 'Payment Page',
 };
 
-exprot defaut function Payment {
+export default function Payment() {
     return(
         <Page display='flex' justifyContent='center' alignItems='center'>
-        <Paymeent />
+        <PaymentForm/>
         </Page>
     );
 }
