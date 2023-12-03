71f280dbe35d0c78f1126fb32be0e03f6ac02e68
sjsh23 <satyasiddhijosh23@gmail.com>
2023-12-03 21:14:14 +0700

    tes
    tes

--- styles/index.tsx
+++ styles/index.tsx
@@ -1,7 +1,7 @@
-'use client';
+"use client";
 
-import { PageProps } from '@/utils/types';
-import styled from 'styled-components';
+import { PageProps } from "@/utils/types";
+import styled from "styled-components";
 
 export const Page = styled.div<PageProps>`
   min-height: 100vh;
@@ -15,7 +15,7 @@ export const NewButton = styled.button`
   width: 100%;
   outline: none;
   border: none;
-  font-family: 'Inter';
+  font-family: "Inter";
   font-size: 1.4rem;
   background-color: #22a39f;
   color: #fff;
@@ -56,7 +56,7 @@ export const InputField = styled.input`
   outline: none;
   border: none;
   color: #fff;
-  font-family: 'Inter';
+  font-family: "Inter";
   font-size: 18px;
   width: 100%;
   box-sizing: border-box;
@@ -111,13 +111,13 @@ export const AboutDiv = styled.div`
   max-width: 800px;
   margin: auto;
   padding: 20px;
-  font-family: 'Inter';
+  font-family: "Inter";
 
   h1 {
     font-size: 2rem;
     font-weight: bold;
     margin-bottom: 20px;
-    font-family: 'Inter';
+    font-family: "Inter";
   }
 `;
 
