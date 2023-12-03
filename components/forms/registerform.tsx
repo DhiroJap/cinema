e947a42627c2a987c74473af5b1283b07cc84cc9
Leeyungsen <frederickmaxwellee02@gmail.com>
2023-12-03 15:24:34 +0700

    test

--- components/forms/registerform.tsx
+++ components/forms/registerform.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { changeGender } from '@/redux/slices/extraSlice';
+import {inputName, inputPhoneNumber, inputEmail, inputPassword, inputBirthDate} from '@/redux/slices/registerSlice';
+import {changeGender} from '@/redux/slices/extraSlice';
 import { AppDispatch, RootState } from '@/redux/store';
 import {
   InputContainer,
@@ -11,55 +12,96 @@ import {
 } from '@/styles';
 import Link from 'next/link';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { FormEvent, FormEventHandler } from 'react';
+import { setUserData } from '@/redux/slices/userSlice';
+import { useRouter } from 'next/navigation';
 
 export default function RegisterForm() {
-  const extra = useSelector((state: RootState) => state.extra);
   const dispatch = useDispatch<AppDispatch>();
+  const {name, phoneNumber, email, password, gender , birthDate} = useSelector(
+    (state: RootState) => state.register
+  )
+  const router = useRouter();
+
+  const registerAction = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('https://localhost:7292/Auth/register', {
+        name: name, 
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password,
+        gender: gender,
+        birthDate: birthDate,
+      });
+
+      const {user} = res.data;
+      dispatch(setUserData(user));
+      router.push('/profile');
+    }catch (error){
+      console.error('Login Failed:', error);
+    }
 
   return (
-    <form className='w-200 flex flex-col gap-2'>
+    <form className='w-200 flex flex-col gap-2' onSubmit= {registerAction}>
       <InputContainer>
-        <InputLabel htmlFor='fullName'>Full Name</InputLabel>
-        <InputField type='text' id='fullName' />
-      </InputContainer>
-
+        <InputLabel htmlFor='name'>Full Name</InputLabel>
+        <InputField id='name' type='text' 
+        onChange={(e) => {
+          dispatch(inputName(e.target.value));
+        }}
+      />
+        
+    </InputContainer>
       <section className='flex justify-between gap-4'>
         <InputContainer>
           <InputLabel htmlFor='phoneNumber'>Phone Number</InputLabel>
-          <InputField type='number' inputMode='numeric' id='phoneNumber' />
+          <InputField id='phoneNumber' type='number' inputMode='numeric' onChange={(e) => {
+            dispatch(inputPhoneNumber(e.target.value));
+          }} 
+          />
         </InputContainer>
 
         <InputContainer className='flex'>
-          <section>
-            <InputLabel htmlFor='gender'>Gender</InputLabel>
-            <RegisterSelect
-              name='gender'
-              id='gender'
-              onChange={(e) => {
-                dispatch(changeGender(e.target.value));
-              }}
-            >
-              <option value='Male'>Male</option>
-              <option value='Female'>Female</option>
+      <section>
+        <InputLabel htmlFor='gender'>Gender</InputLabel>
+          <RegisterSelect
+            name='gender'
+            id='gender'
+            onChange={(e) => {
+            dispatch(changeGender(e.target.value));
+            }}
+          >
+            <option value='Male'>Male</option>
+            <option value='Female'>Female</option>
             </RegisterSelect>
           </section>
-          <h1 className='text-2xl m-auto'>{extra.gender}</h1>
-        </InputContainer>
+          <h1 className='text-2xl m-auto'>{gender}</h1>
+    </InputContainer>
       </section>
 
       <InputContainer>
         <InputLabel htmlFor='birthDate'>Birth Date</InputLabel>
-        <InputField type='date' id='date' />
+        <InputField type='date' id='date' onChange={(e) => {
+          dispatch(inputBirthDate(e.target.value));
+        }}
+        />
       </InputContainer>
 
       <InputContainer>
         <InputLabel htmlFor='email'>Email</InputLabel>
-        <InputField type='email' id='email' />
+        <InputField type='email' id='email' onChange={(e) => {
+          dispatch(inputEmail(e.target.value));
+        }}/>
       </InputContainer>
 
       <InputContainer>
         <InputLabel htmlFor='password'>Password</InputLabel>
-        <InputField type='password' id='password' />
+        <InputField type='password' id='password' onChange={(e) => {
+          dispatch(inputPassword(e.target.value));
+        }}
+        />
       </InputContainer>
 
       <NewButton>Register Account</NewButton>
@@ -70,6 +112,8 @@ export default function RegisterForm() {
           <span className='underline text-customgray-2'>Login</span>
         </Link>
       </div>
+      console.log();
     </form>
   );
+}
 }
--- redux/store.ts
+++ redux/store.ts
@@ -5,12 +5,14 @@ import extraSlice from './slices/extraSlice';
 import loginSlice from './slices/loginSlice';
 import userSlice from './slices/userSlice';
 import moviesSlice from './slices/moviesSlice';
+import registerSlice from './slices/registerSlice'
 export const store = configureStore({
   reducer: {
     extra: extraSlice,
     login: loginSlice,
     user: userSlice,
     movies: moviesSlice,
+    register: registerSlice,
   },
 });
 
