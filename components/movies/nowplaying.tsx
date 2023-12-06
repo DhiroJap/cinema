5f35521d652e82bb6dba21b964b1a8339885446d
JevonSebastian <jevonsebastian952@gmail.com>
2023-12-06 13:51:23 +0700

    fix: fixed img src from server

--- components/movies/nowplaying.tsx
+++ components/movies/nowplaying.tsx
@@ -1,11 +1,10 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
-import { GetNowPlaying } from '@/utils/api';
-import { NowPlayingType } from '@/utils/types';
-import { MoviePlaceholder, MovieStatus } from '@/styles';
-import Link from 'next/link';
-import { pathToPoster } from '@/utils/const';
+import { useEffect, useState } from "react";
+import { GetNowPlaying } from "@/utils/api";
+import { NowPlayingType } from "@/utils/types";
+import { MoviePlaceholder, MovieStatus } from "@/styles";
+import Link from "next/link";
 
 const NowPlaying = () => {
   const [nowPlaying, setNowPlaying] = useState<NowPlayingType[]>([]);
@@ -17,7 +16,7 @@ const NowPlaying = () => {
         const data = await response.json();
         setNowPlaying(data.data);
       } catch (error) {
-        console.error('Error fetching now playing movies:', error);
+        console.error("Error fetching now playing movies:", error);
       }
     };
 
@@ -25,18 +24,18 @@ const NowPlaying = () => {
   }, []);
 
   return (
-    <section className='flex flex-col gap-8 items-start'>
-      <MovieStatus className='bg-secondary text-customwhite-1' disabled>
+    <section className="flex flex-col gap-8 items-start">
+      <MovieStatus className="bg-secondary text-customwhite-1" disabled>
         Now Playing
       </MovieStatus>
-      <div className='grid grid-cols-4 gap-10'>
+      <div className="grid grid-cols-4 gap-10">
         {nowPlaying.map((movie: NowPlayingType) => (
           <MoviePlaceholder key={movie.id}>
             <Link href={`/movies/${movie.id}`}>
               <img
-                src={`${pathToPoster}${movie.poster}`}
+                src={movie.poster}
                 alt={`${movie.title} poster`}
-                className='w-full h-full object-cover'
+                className="w-full h-full object-cover"
               />
             </Link>
           </MoviePlaceholder>
--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,7 @@
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
-import { toast } from 'react-toastify';
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import { AddMovieFormInterface, EditMovieFormInterface } from "./types";
 
 const getNowPlayingMoviesURL: string =
   process.env.NEXT_PUBLIC_GETNOWPLAYINGMOVIES_URL!;
@@ -11,9 +12,13 @@ const getMovieDetailURL: string = process.env.NEXT_PUBLIC_GETMOVIEDETAIL_URL!;
 const getBookingSeatURL: string = process.env.NEXT_PUBLIC_GETBOOKINGSEAT_URL!;
 const getRegisterURL = process.env.NEXT_PUBLIC_GETBOOKINGSEAT_URL;
 const getBookingTimeURL = process.env.NEXT_PUBLIC_GETBOOKINGTIME_URL;
+const deleteMovieURL: string = process.env.NEXT_PUBLIC_DELETEMOVIE_URL!;
+const addMovieURL: string = process.env.NEXT_PUBLIC_ADDMOVIE_URL!;
+const updateMovieURL: string = process.env.NEXT_PUBLIC_UPDATEMOVIE_URL!;
 
 export async function GetNowPlaying() {
   const response = await fetch(getNowPlayingMoviesURL);
+  console.log(response);
   return response;
 }
 
@@ -37,14 +42,14 @@ export const postLogin = async (phoneNumber: string, password: string) => {
 
       if (currentTime - lastErrorTime > COOLDOWN_TIME) {
         toast.error(error.response?.data.message, {
-          position: 'bottom-left',
+          position: "bottom-left",
           autoClose: 5000,
           hideProgressBar: false,
           closeOnClick: true,
           pauseOnHover: true,
           draggable: true,
           progress: undefined,
-          theme: 'dark',
+          theme: "dark",
         });
         lastErrorTime = currentTime;
       }
@@ -77,7 +82,74 @@ export const getBookingSeat = async (scheduleId: number) => {
     console.log(response.data);
     return response.data;
   } catch (error) {
-    throw new Error('Error getting seat: ' + error);
+    throw new Error("Error getting seat: " + error);
+  }
+};
+
+export const addMovie = async (formValue: AddMovieFormInterface) => {
+  try {
+    const formDataForApi = new FormData();
+    formDataForApi.append("posterImage", formValue.poster as File);
+    formDataForApi.append("title", formValue.title);
+    formDataForApi.append("director", formValue.director);
+    formDataForApi.append("synopsis", formValue.synopsis);
+    formDataForApi.append("duration", formValue.duration.toString());
+    formDataForApi.append("releaseDate", formValue.releaseDate);
+    formDataForApi.append("casts", formValue.casts);
+    formDataForApi.append("writer", formValue.writer);
+    formDataForApi.append("rating", formValue.rating);
+
+    const response = await axios.post(addMovieURL, formDataForApi, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    if (response.status === 200) {
+      alert(response.data.data);
+    } else {
+      console.error("Failed to upload movie:", response.statusText);
+    }
+  } catch (error) {
+    console.error("Error during movie upload:", error);
+  }
+};
+
+export const editMovie = async (formValue: EditMovieFormInterface) => {
+  try {
+    const formDataForApi = new FormData();
+    formDataForApi.append("newPoster", formValue.newPoster as File);
+    formDataForApi.append("movieID", formValue.id.toString());
+    formDataForApi.append("newTitle", formValue.newTitle);
+    formDataForApi.append("oldTitle", formValue.oldTitle);
+    formDataForApi.append("director", formValue.director);
+    formDataForApi.append("synopsis", formValue.synopsis);
+    formDataForApi.append("duration", formValue.duration.toString());
+    formDataForApi.append("releaseDate", formValue.releaseDate);
+    formDataForApi.append("casts", formValue.casts);
+    formDataForApi.append("oldPoster", formValue.oldPoster);
+    formDataForApi.append("writer", formValue.writer);
+    formDataForApi.append("rating", formValue.rating);
+
+    const response = await axios.put(updateMovieURL, formDataForApi, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+
+    if (response.status === 200) {
+      alert(response.data.data);
+    } else {
+      console.error("Failed to upload movie:", response.statusText);
+    }
+  } catch (error) {
+    console.error("Error during movie upload:", error);
+  }
+};
+
+export const deleteMovie = async (movieId: number) => {
+  try {
+    const url = deleteMovieURL + "/" + movieId;
+    const response = await axios.delete(url);
+    return response.data;
+  } catch (error: any) {
+    return error.response?.data;
   }
 };
 
@@ -100,7 +172,7 @@ export const postRegister = async (
     });
     return response.data;
   } catch (error) {
-    throw new Error('Error registering you: ' + error);
+    throw new Error("Error registering you: " + error);
   }
 };
 
@@ -109,6 +181,6 @@ export async function GetBookingTime(id: string) {
     const response = await fetch(`${getBookingTimeURL}${id}`);
     return response;
   } catch (error) {
-    console.error('Error fetching schedules:', error);
+    console.error("Error fetching schedules:", error);
   }
 }
